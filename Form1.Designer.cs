550a55dd027c3fddbd7b6e2b7061515f9de7b22f
Ice <your name>
2020-02-03 22:02:02 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Photo_Print_Order_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxSize = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.Price = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.quantity = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Size";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbxSize
+            // 
+            this.cbxSize.FormattingEnabled = true;
+            this.cbxSize.Location = new System.Drawing.Point(232, 64);
+            this.cbxSize.Name = "cbxSize";
+            this.cbxSize.Size = new System.Drawing.Size(121, 21);
+            this.cbxSize.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(127, 153);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(131, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate Price";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Price
+            // 
+            this.Price.AutoSize = true;
+            this.Price.Location = new System.Drawing.Point(94, 193);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(31, 13);
+            this.Price.TabIndex = 3;
+            this.Price.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(232, 193);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // quantity
+            // 
+            this.quantity.AutoSize = true;
+            this.quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantity.Location = new System.Drawing.Point(94, 113);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(61, 17);
+            this.quantity.TabIndex = 5;
+            this.quantity.Text = "Quantity";
+            this.quantity.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(232, 106);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtQuantity.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cbxSize);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Photo Order";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxSize;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label Price;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label quantity;
+        private System.Windows.Forms.TextBox txtQuantity;
+    }
+}
+
